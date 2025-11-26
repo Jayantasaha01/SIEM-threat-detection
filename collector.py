967340d94df1c2481ef06178b5587fe43e1a0962
Jayantasaha01 <144397397+Jayantasaha01@users.noreply.github.com>
2025-11-27 02:42:07 +0600

    Create collector.py

---
+++ collector.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import json, time, random
+
+IPS = ["192.168.1.15", "10.0.0.5", "203.0.113.45", "198.51.100.77"]
+EVENTS = ["login_success", "login_failed", "file_access", "network_scan"]
+
+while True:
+    log = {
+        "timestamp": time.time(),
+        "src_ip": random.choice(IPS),
+        "event": random.choice(EVENTS)
+    }
+    with open("logs/sample_logs.json", "a") as f:
+        f.write(json.dumps(log) + "\n")
+    time.sleep(1)
